eefe5a2430761db0b9bae0720bcc91a3f29c8649
Coder-YogeshJi <147072802+Coder-YogeshJi@users.noreply.github.com>
2024-01-04 20:54:24 +0530

    Update new_try.py

--- new_try.py
+++ new_try.py
@@ -5,7 +5,7 @@ image_path='image.jpg'
 min_confidence=0.2 
 
 classes = ["person",
-"bicycle","car","motorbike","aeroplane","bus","train","truck","boat","traffic light","fire"hydrant","stop sign","parking meter","bench","bird","cat","dog","horse","sheep","cow","elephant","bear","zebra","giraffe","backpack","umbrella","handbag","tie","suitcase","frisbee","skis","snowboard","sports ball","kite","baseball bat","baseball glove","skateboard","surfboard","tennis racket","bottle","wine glass","cup","fork","knife","spoon","bowl","banana","apple","sandwich","orange","broccoli","carrot",
+"bicycle","car","motorbike","aeroplane","bus","train","truck","boat","traffic light","fire","hydrant","stop sign","parking meter","bench","bird","cat","dog","horse","sheep","cow","elephant","bear","zebra","giraffe","backpack","umbrella","handbag","tie","suitcase","frisbee","skis","snowboard","sports ball","kite","baseball bat","baseball glove","skateboard","surfboard","tennis racket","bottle","wine glass","cup","fork","knife","spoon","bowl","banana","apple","sandwich","orange","broccoli","carrot",
 "hot dog","pizza","donut","cake","chair","sofa","pottedplant","bed","dining table","toilet","tvmonitor","laptop","mouse","remote","keyboard","cell phone","microwave","oven","toaster","sink","refrigerator","book","clock","vase",'scissors","teddy bear","hair drier","toothbrush"]
 
 np.random.seed(54321)
